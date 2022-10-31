254cff5985550d828d1b1f67a984ee71389961cb
Jason Lee <huacnlee@gmail.com>
2022-10-31 23:11:39 +0800

    Improve Lint annotation style for error use orange, warning use cyan color.

--- src/main/java/io/github/huacnlee/autocorrectIdeaPlugin/AutoCorrectTextAttributes.kt
+++ src/main/java/io/github/huacnlee/autocorrectIdeaPlugin/AutoCorrectTextAttributes.kt
@@ -1,19 +1,22 @@
 package io.github.huacnlee.autocorrectIdeaPlugin
 
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.markup.EffectType
 import com.intellij.openapi.editor.markup.TextAttributes
 import java.awt.Color
 
 class AutoCorrectTextAttributes {
     fun errorAttribute(): TextAttributes {
-        val attribute = DefaultLanguageHighlighterColors.INVALID_STRING_ESCAPE.defaultAttributes.clone()
-        attribute.effectColor = Color.YELLOW
+        val attribute = TextAttributes();
+        attribute.effectColor = Color.ORANGE
+        attribute.effectType = EffectType.WAVE_UNDERSCORE
         return attribute
     }
 
     fun warningAttribute(): TextAttributes {
-        val attribute = DefaultLanguageHighlighterColors.INVALID_STRING_ESCAPE.defaultAttributes.clone()
+        val attribute = TextAttributes()
         attribute.effectColor = Color.CYAN
+        attribute.effectType = EffectType.WAVE_UNDERSCORE
         return attribute
     }
 }
